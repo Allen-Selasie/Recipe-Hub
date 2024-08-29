d3507d063284ce9bd966f6f71704bdbb91b9b28a
Allen-Selasie <allen13selasie@gmail.com>
2024-08-29 11:09:44 +0000

    feat: Update reset password link to include protocol and host

--- Server/routes/userRoutes.js
+++ Server/routes/userRoutes.js
@@ -173,7 +173,7 @@ UserRoutes.post("/forgotPassword", async (req, res) => {
         await user.save();
 
         // Compose the reset password email
-        const resetPasswordLink = `localhost:2000/u/reset/${token}`;
+        const resetPasswordLink = `${req.protocol}://${req.get("host")}/u/reset/${token}`;
         const message = {
             html: `<p>Click the following link to reset your password:</p><a href="${resetPasswordLink}">${resetPasswordLink}</a>`,
             text: `Copy and paste the following link in your browser to reset your password: ${resetPasswordLink}`,
